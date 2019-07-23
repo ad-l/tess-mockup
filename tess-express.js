83430ed999803b76ada6821d10e91c64eab245b2
Ercan Ozturk <t-eroztu@microsoft.com>
2019-07-23 09:57:52 +0100

    Add delete repo API

--- tess-express.js
+++ tess-express.js
@@ -22,6 +22,7 @@ const EP_EDIT_REPO = "/repos/:owner/:repo";
 // Github User
 GITHUB_USER_TOKEN = "072d3445cf3bc85d165e85b19f6a40fa55fccef2";
 GITHUB_USER_AGENT = "TESS";
+GITHUB_USER = "transparent-enclave";
 
 app.post(EP_CREATE_REPO, function (req, res) {
     console.log("\n\n\n\n\n\n\n\n\n\n");
@@ -57,7 +58,32 @@ app.post(EP_CREATE_REPO, function (req, res) {
 app.delete(EP_DELETE_REPO, function (req, res) {
     console.log(req);
     console.log("Delete repo request received. Owner: " + req.params.owner + ", name: " + req.params.repo);
-    res.send("OK");
+    // Post!
+    request.delete({
+        url: "https://api.github.com" + EP_DELETE_REPO.replace(":owner", req.params.owner).replace(":repo", req.params.repo),
+        headers: {
+            "Authorization": "token " + GITHUB_USER_TOKEN,
+            "User-Agent": GITHUB_USER_AGENT,
+            "content-type": "application/json"
+        },
+    },
+        // Handle Github response
+        function (error, response, body) {
+            if (error) {
+                console.log("Error occured:");
+                console.log(error);
+                res.send(body);
+            } else {
+                console.log("Status code: " + response.statusCode);
+                if (response.statusCode == 204) {
+                    console.log("Successfully deleted.");
+                    res.send(body);
+                }
+                console.log("Github response body:")
+                console.log(body);
+            }
+        });
+
 })
 
 app.patch(EP_EDIT_REPO, function (req, res) {
