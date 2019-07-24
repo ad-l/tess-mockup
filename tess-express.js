b7313bd14f036d823c6699eb7e5d32458c8405ba
Ercan Ozturk <t-eroztu@microsoft.com>
2019-07-24 09:29:19 +0100

    Add lost in merge collaborator related proxies

--- tess-express.js
+++ tess-express.js
@@ -257,6 +257,76 @@ app.patch(EP_EDIT_REPO, function (req, res) {
         });
 })
 
+app.put(EP_ADD_COLLAB, function (req, res) {
+    console.log(req);
+    console.log("Add collaborator request received. Owner: " + req.params.owner + ", repo: " + req.params.repo +
+                ", username: " + req.params.username);
+    console.log("Body: ");
+    console.log(req.body);
+    request.put({
+        url: "https://api.github.com" + EP_ADD_COLLAB.replace(":owner", req.params.owner).replace(":repo", req.params.repo)
+                                                    .replace(":username", req.params.username),
+        headers: {
+            "Authorization": "token " + GITHUB_USER_TOKEN,
+            "User-Agent": GITHUB_USER_AGENT,
+            "content-type": "application/json"
+        },
+    },
+        // Handle Github response
+        function (error, response, body) {
+            if (error) {
+                console.log("Error occured:");
+                console.log(error);
+                res.send(body);
+            } else {
+                console.log("Status code: " + response.statusCode);
+                if (response.statusCode == 201) {
+                    console.log("Successfully added collaborator.");
+                    res.send(body);
+                } else if (response.statusCode == 204) {
+                    console.log("Already a collaborator.");
+                    res.send(body);
+                }
+                console.log("Github response body:")
+                console.log(body);
+            }
+        });
+})
+
+app.delete(EP_REMOVE_COLLAB, function (req, res) {
+    console.log(req);
+    console.log("Remove collaborator request received. Owner: " + req.params.owner + ", repo: " + req.params.repo +
+                ", username: " + req.params.username);
+    console.log("Body: ");
+    console.log(req.body);
+    request.delete({
+        url: "https://api.github.com" + EP_REMOVE_COLLAB.replace(":owner", req.params.owner).replace(":repo", req.params.repo)
+                                                    .replace(":username", req.params.username),
+        headers: {
+            "Authorization": "token " + GITHUB_USER_TOKEN,
+            "User-Agent": GITHUB_USER_AGENT,
+            "content-type": "application/json"
+        },
+    },
+        // Handle Github response
+        function (error, response, body) {
+            if (error) {
+                console.log("Error occured:");
+                console.log(error);
+                res.send(body);
+            } else {
+                console.log("Status code: " + response.statusCode);
+                if (response.statusCode == 204) {
+                    console.log("Collaborator deleted.");
+                    res.send(body);
+                }
+                console.log("Github response body:")
+                console.log(body);
+            }
+        });
+})
+
+
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////
